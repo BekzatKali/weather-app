03cef9563a63dc6e1405b44bf06074a14d65ee15
Bekzat <kalibekzat2003@gmail.com>
2024-02-20 13:08:33 +0600

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=53ed1a12294bf794e6891a613bb458e8`;
+
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -16,6 +16,7 @@ function App() {
       setLocation('')
     }
   }
+
 
   let answerTemp;
   let answerFeelsLike;
@@ -29,7 +30,7 @@ function App() {
   }
 
   return (
-    <div className='app py-20 text-center'>
+    <div className='app py-20 text-center overflow-hidden'>
       <div className='container flex flex-col justify-between items-center'>
         <div>
           <input type="text" value={location} onChange={event => setLocation(event.target.value)} placeholder='Enter Location' onKeyDown={searchLocation} className='py-3 px-8 rounded-md input'/>
@@ -61,3 +62,5 @@ function App() {
 }
 
 export default App
+
+
